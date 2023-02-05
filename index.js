10b07ef9d89d48cd0fba5c89316ab2148b856d33
Dev Gupta <guptadev265@gmail.com>
2023-02-06 01:23:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/products", productRouter);
 app.use(cookieParser());
 
 db.sequelize.sync().then(() => {
-  app.listen(8080, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}.`);
   });
 });
