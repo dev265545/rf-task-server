6721568be8005dcf5d23a6e00bc3c58d096182f4
Dev Gupta <guptadev265@gmail.com>
2023-02-06 01:20:50 +0530

    FIX ERRORS

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/products", productRouter);
 app.use(cookieParser());
 
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(8080, () => {
     console.log(`Server is running on port ${PORT}.`);
   });
 });
