32564c0bb9e1bb3f72c972dc3ede969dea01d7b4
Dev Gupta <guptadev265@gmail.com>
2023-02-06 02:45:15 +0530

    s

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const db = require("./models");
 const userRoutes = require("./Routes/userRoute");
 const productRouter = require("./Routes/Product");
 //setting up your port
-const PORT = process.env.PORT || 8080;
+const PORT =  7361;
 
 //assigning the variable app to express
 const app = express();
@@ -25,20 +25,17 @@ app.use(cookieParser());
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("db has been re sync");
 // });
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+
 //routes for the user API
 app.use("/users", userRoutes);
 app.use("/products", productRouter);
 app.use(cookieParser());
-db.sequelize
-  .authenticate()
-  .then(() => {
-     app.listen(process.env.PORT, () => {
-       console.log(`Server is running on port ${process.env.PORT}.`);
-     });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+app.get("/", (req, res) => {
+  res.send("GET Request Called");
+});
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
   });
+
+});
